855b677d79639ca0fbc01191fe10d0809fdd170e
Murat Serdar Akkuş <muratserdarakkus@Murats-MacBook-Air.local>
2021-05-10 03:59:30 +0300

    Process Fix

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.post("/",verify_request,(req,res)=>{
         return res.status(403).send("Unauthorized");
     }
     
-    const ls = spawn('pm2',["restart","all"]);
+    const ls = spawn('pm2',["restart",process.env.APP_NAME || "all"]);
 
     ls.stdout.on('data', (data) => {
         console.log(`stdout: ${data}`);
